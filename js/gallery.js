a4b3330336bcb064204a2be62782a4e89b31828c
Andrew Huntington <andrew.huntington@gmail.com>
2021-03-09 23:38:36 +0900

    Preload galley images with JavaScript

--- js/gallery.js
+++ js/gallery.js
@@ -11,9 +11,13 @@ function activateGallery() {
 
   // Set clicked image as main image.
   thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
       // Set clicked image as display image.
-      let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", thumbnail.alt);
 
