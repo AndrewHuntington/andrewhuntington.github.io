00888dbdbf3d79e66dbe89971c497e925a03e399
Andrew Huntington <andrew.huntington@gmail.com>
2021-03-09 22:42:24 +0900

    Add ability to change gallery image

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,18 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  
+  // Set clicked image as main image.
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.alt
+
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+    });
+  });
 }
